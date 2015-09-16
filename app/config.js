050a76f99313c934948c33df886b5d52360b65c6
Fareez Ahmed <Fareez.Ahmed@gmail.com>
2015-09-15 20:48:02 -0700

    updated

--- app/config.js
+++ app/config.js
@@ -2,45 +2,45 @@ var Bookshelf = require('bookshelf');
 var path = require('path');
 
 var db = Bookshelf.initialize({
-  client: 'sqlite3',
-  connection: {
-    host: '127.0.0.1',
-    user: 'your_database_user',
-    password: 'password',
-    database: 'shortlydb',
-    charset: 'utf8',
-    debug: true,
-    filename: path.join(__dirname, '../db/shortly.sqlite')
-  }
+    client: 'sqlite3',
+    connection: {
+        host: '127.0.0.1',
+        user: 'your_database_user',
+        password: 'password',
+        database: 'shortlydb',
+        charset: 'utf8',
+        debug: true,
+        filename: path.join(__dirname, '../db/shortly.sqlite')
+    }
 });
 
 db.knex.schema.hasTable('urls').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('urls', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('base_url', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('user_id');
-      link.integer('visits');
-      link.timestamps();
-    }).then(function() {
-      console.log('Created URLs Table');
-    });
-  }
+    if (!exists) {
+        db.knex.schema.createTable('urls', function(link) {
+            link.increments('id').primary();
+            link.string('url', 255);
+            link.string('base_url', 255);
+            link.string('code', 100);
+            link.string('title', 255);
+            link.integer('user_id');
+            link.integer('visits');
+            link.timestamps();
+        }).then(function() {
+            console.log('Created URLs Table');
+        });
+    }
 });
 
 db.knex.schema.hasTable('clicks').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('clicks', function (click) {
-      click.increments('id').primary();
-      click.integer('link_id');
-      click.timestamps();
-    }).then(function() {
-      console.log('Created Clicks Table');
-    });
-  }
+    if (!exists) {
+        db.knex.schema.createTable('clicks', function(click) {
+            click.increments('id').primary();
+            click.integer('link_id');
+            click.timestamps();
+        }).then(function() {
+            console.log('Created Clicks Table');
+        });
+    }
 });
 
 /************************************************************/
@@ -48,16 +48,16 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
 /************************************************************/
 
 db.knex.schema.hasTable('users').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 32);
-      user.string('password', 128);
-      user.timestamps();
-    }).then(function() {
-      console.log('Created Users Table');
-    });
-  }
+    if (!exists) {
+        db.knex.schema.createTable('users', function(user) {
+            user.increments('id').primary();
+            user.string('username', 200);
+            user.string('password', 200);
+            user.timestamps();
+        }).then(function() {
+            console.log('Created Users Table');
+        });
+    }
 });
 
 
